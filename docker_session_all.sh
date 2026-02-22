88195ddd7f4ed003053bcc568e9627aa0b1eb390
marcin.matecki <matecki.m@outlook.com>
2026-02-22 15:59:58 +0100

    first com

---
+++ docker_session_all.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage:"
+  echo "  $0 <ros1_bag> <ros2_bag_dir> <output_dir>"
+  echo
+  echo "ros1_bag      : path to a ROS1 .bag file"
+  echo "ros2_bag_dir  : path to a ROS2 bag directory"
+  echo "output_dir    : directory to store outputs"
+  exit 1
+}
+
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    usage
+fi
+
+if [[ $# -ne 3 ]]; then
+    usage
+fi
+
+ROS1_BAG=$(realpath "$1")
+ROS2_BAG_DIR=$(realpath "$2")
+OUTPUT_DIR=$(realpath "$3")
+
+mkdir -p "$OUTPUT_DIR"
+
+CLONE_DIR="$HOME/hdmapping-benchmark"
+
+ROS1_REPOS=(
+"benchmark-DLO-to-HDMapping"
+"benchmark-Super-LIO-to-HDMapping"
+"benchmark-DLIO-to-HDMapping"
+"benchmark-FAST-LIO-to-HDMapping"
+"benchmark-Faster-LIO-to-HDMapping"
+"benchmark-iG-LIO-to-HDMapping"
+"benchmark-I2EKF-LO-to-HDMapping"
+"benchmark-CT-ICP-to-HDMapping"
+"benchmark-LOAM-Livox-to-HDMapping"
+"benchmark-SLICT-to-HDMapping"
+"benchmark-LIO-EKF-to-HDMapping"
+"benchmark-LeGO-LOAM-to-HDMapping"
+"benchmark-Point-LIO-to-HDMapping"
+"benchmark-VoxelMap-to-HDMapping"
+)
+
+ROS2_REPOS=(
+"benchmark-SuperOdometry-to-HDMapping"
+"benchmark-KISS-ICP-to-HDMapping"
+"benchmark-GenZ-ICP-to-HDMapping"
+"benchmark-GLIM-to-HDMapping"
+"benchmark-RESPLE-to-HDMapping"
+"benchmark-lidar_odometry_ros_wrapper-to-HDMapping"
+"benchmark-mola_lidar_odometry-to-HDMapping"
+)
+
+ROS1_ALGOS=(
+  "dlo"
+  "super-lio"
+  "dlio"
+  "fast-lio"
+  "faster-lio"
+  "ig-lio"
+  "i2ekf-lo"
+  "ct-icp"
+  "loam"
+  "slict"
+  "lio-ekf"
+  "lego-loam"
+  "point-lio"
+  "voxel-map"
+)
+for i in "${!ROS1_ALGOS[@]}"; do
+    algo="${ROS1_ALGOS[$i]}"
+    repo="${ROS1_REPOS[$i]}"
+    OUTPUT="$OUTPUT_DIR/$algo"
+    mkdir -p "$OUTPUT"
+
+    # Wybór inputu
+    if [[ "$algo" == "dlio" || "$algo" == "dlo" || "$algo" == "loam" || "$algo" == "ct-icp" || "$algo" == "lego-loam" || "$algo" == "lio-ekf" ]]; then
+        INPUT="${ROS1_BAG}-pc"
+    else
+        INPUT="$ROS1_BAG"
+    fi
+
+    echo "=== Waiting 5 seconds before running $algo ==="
+    sleep 5
+
+    cd "$CLONE_DIR/$repo"
+    echo "=== Running $algo in $(pwd) on $INPUT ==="
+    ./docker_session_run-ros1-"$algo".sh "$INPUT" "$OUTPUT"
+
+    cd "$CLONE_DIR"
+    echo "=== Finished $algo ==="
+done
+
+for i in "${!ROS2_ALGOS[@]}"; do
+    algo="${ROS2_ALGOS[$i]}"
+    repo="${ROS2_REPOS[$i]}"
+    OUTPUT="$OUTPUT_DIR/$algo"
+    mkdir -p "$OUTPUT"
+
+    if [[ "$algo" == "resple" ]]; then
+        INPUT="${ROS2_BAG_DIR}-lidar"
+    else
+        INPUT="$ROS2_BAG_DIR"
+    fi
+
+    echo "=== Waiting 5 seconds before running $algo ==="
+    sleep 5
+
+    cd "$CLONE_DIR/$repo"
+    echo "=== Running $algo in $(pwd) on $INPUT ==="
+    ./docker_session_run-ros2-"$algo".sh "$INPUT" "$OUTPUT"
+
+    cd "$CLONE_DIR"
+    echo "=== Finished $algo ==="
+done
